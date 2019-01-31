f71e40bb5f9b274f3f039ed27a5e5eb55b842504
LukasDombrovsky <lukas.dombrovsky@gmail.com>
2019-02-01 00:03:22 +0100

    new mainpage and functional twitterInsights

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,8 +1,35 @@
-app.controller('MainController', ['$scope','$location',
- function($scope,$location) {
+app.controller('MainController', ['$scope','$location','$document','$timeout','$css',
+ function($scope,$location,$document,$timeout,$css) {
+    // Apending custom stylesheets 
+    // angular.element(document.querySelector('head')).append('<link href="../../css/mainPage_style.css" rel="stylesheet">');
+
+    // angular.element(document.querySelector('.titanic naked')).append('<link href="../../css/mainPage_style.css" rel="stylesheet">');
+    // transform: translate(-50%, -50%);
+
+    // $timeout( function(){
+    //     MainPage();
+    // }, 2000);
+
+
+    // $document.ready(function(){
+    //     MainPage();
+    // });
+
+    // Calling MainPage JavaScript fucntion when this controlled is fully loaded
+    this.$onInit = function () {
+        MainPage();
+    }
 
     $scope.openApp = function(appName) {
         $location.path('/' + appName);
     };
+
+    // angular.element(document.querySelector(".titanic")).css('transform', 'translate(-50%, -50%)');
+
+    // TweenMax.from(hi, 5, {
+	// 	y: 140,
+	// 	opacity: 0,
+	// 	ease: Elastic.easeOut
+	// })
  
 }]);
